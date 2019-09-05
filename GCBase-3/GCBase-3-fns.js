a106e6ed88edc4ecd0fae84e4ac7e418b8c24a41
storktec@mail.ru <storktec@mail.ru>
2019-09-05 18:11:04 +0300

    Создана отдельная папка проекта. Файл для функций

---
+++ GCBase-3/GCBase-3-fns.js
@@ -0,0 +1,35 @@
+/*jshint esversion: 6 */
+/*jshint browser: true */
+
+"use strict";
+
+let base = {
+	students: [
+		{ id: 0, FIO: "Ivan",	grade: "III"},
+		{ id: 1, FIO: "Maria",	grade: "II"	},
+		{ id: 2, FIO: "Petr",	grade: "I"	},
+		{ id: 3, FIO: "Anton",	grade: "IV"	},
+		{ id: 4, FIO: "Julia",	grade: "V"	}
+	],
+
+	trainings: [
+		{ id: 0, name: "BME"},
+		{ id: 1, name: "Sales"},
+		{ id: 2, name: "Defeat"},
+		{ id: 3, name: "TM"},
+		{ id: 4, name: "Stress"},
+		{ id: 5, name: "Eloquent speaker"}
+	],
+
+	events: [
+		{ id: 0, date: "2019-09-01", theme: 5, students: [2, 3] },
+		{ id: 1, date: "2019-08-01", theme: 4, students: [0, 1, 3] },
+		{ id: 2, date: "2019-07-01", theme: 2, students: [1, 0] },
+		{ id: 3, date: "2019-06-01", theme: 5, students: [0, 3, 2] }
+	],
+
+	__relations: [
+		{table: "events", field: "students", type: "one-to-many", toTable: "students", byField: "id"},
+		{table: "events", field: "theme", type: "one-to-one", toTable: "trainings", byField: "id"}
+	]
+};
